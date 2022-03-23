03b3527a06105b485737f0437c5b66289cd1e75c
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2022-03-23 12:53:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const display = () =>{
     document.getElementById("emoji").style.visibility = "visible";
+    const collection = document.getElementsByClassName("emoji");
+    for (let i = 0; i < collection.length; i++) {
+    collection[i].style.animation = "popup";
+    collection[i].style.animationDuration = "0.3s";
+    collection[i].style.animationDelay = `${i*0.05}s`
+    }
 }
 const hide = () =>{
     document.getElementById("emoji").style.visibility = "hidden";
+    const collection = document.getElementsByClassName("emoji");
+    for (let i = 0; i < collection.length; i++) {
+    collection[i].style.animation = "";
+    }
 }
