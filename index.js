15da0256011525bb98c3cc23e5f1a0cfa374ad7b
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2022-03-23 11:49:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const display = () =>{
+    document.getElementById("emoji").style.visibility = "visible";
+}
+const hide = () =>{
+    document.getElementById("emoji").style.visibility = "hidden";
+}
